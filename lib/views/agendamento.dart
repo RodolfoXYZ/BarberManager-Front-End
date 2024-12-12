85648dcbd740239351387d88fda767c133618b2d
LucasM2010 <131216534+LucasM2010@users.noreply.github.com>
2024-12-12 15:34:03 -0300

    Adicionando retorno de conteúdo para tela de confirmação

--- lib/views/agendamento.dart
+++ lib/views/agendamento.dart
@@ -1,14 +1,15 @@
 import 'package:barbershop_app/views/FormaDePagamento.dart';
 import 'package:flutter/material.dart';
-import 'ConfirmacaoAgendamento.dart';
 
 class AgendamentoPage extends StatefulWidget {
   final Map<String, bool> selectedServices;
   final String? serviceLocation;
+  final Map<String, String> selectedBarber;
 
   AgendamentoPage({
     required this.selectedServices,
     required this.serviceLocation,
+    required this.selectedBarber,
   });
 
   @override
@@ -46,7 +47,11 @@ class _AgendamentoPageState extends State<AgendamentoPage> {
 
   @override
   Widget build(BuildContext context) {
+    // Usando os dados passados da BarberSelectionPage
+    final selectedBarber = widget.selectedBarber;
+
     return Scaffold(
+      resizeToAvoidBottomInset: true, // Ajusta o layout ao aparecer o teclado
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 18, 2, 66),
@@ -62,190 +67,186 @@ class _AgendamentoPageState extends State<AgendamentoPage> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // Exibindo os serviços selecionados
+            Container(
               padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade300,
+                    blurRadius: 6.0,
+                    offset: const Offset(0, 4),
+                  ),
+                ],
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Container(
-                    padding: const EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.shade300,
-                          blurRadius: 6.0,
-                          offset: const Offset(0, 4),
-                        ),
-                      ],
+                  Text(
+                    'Serviço(s) Selecionado(s):',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  for (var entry in widget.selectedServices.entries)
+                    if (entry.value)
+                      Text(entry.key, style: TextStyle(fontSize: 14)),
+                  SizedBox(height: 20),
+                  Text(
+                    'Local de Serviço: ${widget.serviceLocation ?? 'Não especificado'}',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            // Exibindo a seleção da data
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade300,
+                    blurRadius: 6.0,
+                    offset: const Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Selecione a data disponível',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () => _pickDate(context),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey[200],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          'Serviço(s) Selecionado(s):',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 10),
-                        for (var entry in widget.selectedServices.entries)
-                          if (entry.value)
-                            Text(entry.key, style: TextStyle(fontSize: 14)),
-                        SizedBox(height: 20),
-                        Text(
-                          'Local de Serviço: ${widget.serviceLocation ?? 'Não especificado'}',
-                          style: TextStyle(fontSize: 14),
-                        ),
-                      ],
+                    child: Text(
+                      _selectedDate == null
+                          ? 'Escolha uma data'
+                          : 'Data selecionada: ${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}',
+                      style: TextStyle(color: Colors.black),
                     ),
                   ),
-                  SizedBox(height: 20),
-                  Container(
-                    padding: const EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.shade300,
-                          blurRadius: 6.0,
-                          offset: const Offset(0, 4),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          'Selecione datas disponíveis',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 10),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            // Exibindo os horários disponíveis
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade300,
+                    blurRadius: 6.0,
+                    offset: const Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Horários Disponíveis',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Wrap(
+                    spacing: 8.0,
+                    runSpacing: 8.0,
+                    children: [
+                      for (String time in [
+                        '09:00',
+                        '10:00',
+                        '11:00',
+                        '13:00',
+                        '14:00',
+                        '15:00'
+                      ])
                         ElevatedButton(
-                          onPressed: () => _pickDate(context),
+                          onPressed: () => _selectTime(time),
                           style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.grey[200],
+                            backgroundColor: _selectedTime == time
+                                ? Colors.blue
+                                : Colors.grey[200],
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(8.0),
                             ),
                           ),
                           child: Text(
-                            _selectedDate == null
-                                ? 'Escolha uma data'
-                                : 'Data selecionada: ${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}',
-                            style: TextStyle(color: Colors.black),
+                            time,
+                            style: TextStyle(
+                                color: _selectedTime == time
+                                    ? Colors.white
+                                    : Colors.black),
                           ),
                         ),
-                      ],
-                    ),
+                    ],
                   ),
-                  SizedBox(height: 20),
-                  Container(
-                    padding: const EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.shade300,
-                          blurRadius: 6.0,
-                          offset: const Offset(0, 4),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          'Horários Disponíveis',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 10),
-                        Wrap(
-                          spacing: 8.0,
-                          runSpacing: 8.0,
-                          children: [
-                            for (String time in [
-                              '09:00',
-                              '10:00',
-                              '11:00',
-                              '13:00',
-                              '14:00',
-                              '15:00'
-                            ])
-                              ElevatedButton(
-                                onPressed: () => _selectTime(time),
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: _selectedTime == time
-                                      ? Colors.blue
-                                      : Colors.grey[200],
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(8.0),
-                                  ),
-                                ),
-                                child: Text(
-                                  time,
-                                  style: TextStyle(
-                                      color: _selectedTime == time
-                                          ? Colors.white
-                                          : Colors.black),
-                                ),
-                              ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 20),
                 ],
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: _selectedDate != null && _selectedTime != null
-                  ? () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => PaymentPage(
-                            selectedDate: _selectedDate!,
-                            selectedTime: _selectedTime!,
-                            selectedServices: widget.selectedServices,
-                            serviceLocation: widget.serviceLocation,
+            SizedBox(height: 20),
+            // Botão para finalizar o agendamento
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: _selectedDate != null && _selectedTime != null
+                    ? () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => PaymentPage(
+                              selectedDate: _selectedDate!,
+                              selectedTime: _selectedTime!,
+                              selectedServices: widget.selectedServices,
+                              serviceLocation: widget.serviceLocation,
+                              selectedBarber:
+                                  selectedBarber, // Passando os dados do barbeiro
+                            ),
                           ),
-                        ),
-                      );
-                    }
-                  : null,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 18, 2, 66),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+                        );
+                      }
+                    : null,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 18, 2, 66),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
                 ),
-              ),
-              child: SizedBox(
-                height: 50,
-                child: Center(
-                  child: Text(
-                    'FINALIZAR',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
+                child: SizedBox(
+                  height: 50,
+                  child: Center(
+                    child: Text(
+                      'FINALIZAR',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
